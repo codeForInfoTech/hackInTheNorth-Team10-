9d230bcbe8e406e033bb4b43fe0bd8412adeda6d
Rohan <43247738+RohanBolusani@users.noreply.github.com>
2018-09-16 18:21:44 +0530

    Add files via upload

--- website.py
+++ website.py
@@ -1,26 +1,21 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, jsonify, request
 from forms import RegistrationForm, LoginForm
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY']= '1d2557964bea6d85898400f32419ebcc'
+app.config['MONGO_DBNAME'] = 'studentlist'
+app.config['MONGO_URI'] ='mongodb://admin:password1@ds257752.mlab.com:57752/studentlist'
 
-posts = [{
-       'name':'Rohan',
-       'Age':'19'    
-},
-{
-    'name':'Rohit',
-    'Age':'19'  
-}
-]
+mongo=PyMongo(app)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 
 @app.route("/about")
@@ -47,6 +42,17 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/chart')
+def chart():
+    return render_template('charts.html')  
+
+@app.route('/data')
+def data():
+    id = request.args.get('id')
+    student = mongo.db.charts
+    result = student.find_one({'name': id})
+    return jsonify({'results' : result['values']})
 
 
 if __name__ == '__main__':
